aec8fee2c8c80a055db83d567be3d59597c77bfb
Abhyudaya <abhyudaya@live.co.uk>
2019-11-08 19:22:39 -0500

    Base commit
    - Add pre-provided files for MPI shuffle

---
+++ a1.hpp
@@ -0,0 +1,17 @@
+/*  YOUR_FIRST_NAME
+ *  YOUR_LAST_NAME
+ *  YOUR_UBIT_NAME
+ */
+
+#ifndef A1_HPP
+#define A1_HPP
+
+#include <vector>
+
+
+template <typename T, typename Hash>
+void mpi_shuffle(std::vector<T>& inout, Hash hash, MPI_Datatype Type, MPI_Comm Comm) {
+
+} // mpi_shuffle
+
+#endif // A1_HPP
