c20e1b14034e6107e9397fc03f4fbe1438e70469
Abhyudaya <abhyudaya@live.co.uk>
2019-11-10 16:13:56 -0500

    Implement send/receive count and displacement
    - Implement MPI_SendRecv() to communicate the total no of items each
    processor will need to receive from other processors
    - Calculate the displacements for items in the receive buffer

--- a1.hpp
+++ a1.hpp
@@ -25,8 +25,9 @@ void mpi_shuffle(std::vector<T>& inout, Hash hash, MPI_Datatype Type, MPI_Comm C
     2. Calculate no of elements each processor has for each other processor
     3. Sort inout based on hash % p so that displacement can be calculated
     4. Calculate displacement in inout for each other processor to send
-    5. Ready the receive buffer same as send buffer above
-    6. MPI_alltoallv() with send and receive buffer details
+    5. Send and receive the no of items for each processor to be received
+    6. Prepare the displacement vector for storing the offsets when receiving
+    7. MPI_alltoallv() with send and receive buffer details
     */
 
     /*
@@ -40,6 +41,9 @@ void mpi_shuffle(std::vector<T>& inout, Hash hash, MPI_Datatype Type, MPI_Comm C
 
     std::vector<int> countVec(size, 0);
     std::vector<int> displacementVec(size, 0);
+    std::vector<int> recCountVec(size, 0);
+    int totalRecCount = 0;
+    int totalSendCount = 0;
 
 /*
     --- Possibly useless code ---
@@ -61,8 +65,8 @@ void mpi_shuffle(std::vector<T>& inout, Hash hash, MPI_Datatype Type, MPI_Comm C
 
     // 1. Print length of each processor's inout
     std::cout << "[" << rank << "] Length of inout: " << inout.size() << "\n";
-
     MPI_Barrier(Comm); // DEBUG: Step 1
+
 
     // 2. Calculate no of elements each processor has for each other processor
     for(int i = 0; i < inout.size(); i++)
@@ -77,8 +81,8 @@ void mpi_shuffle(std::vector<T>& inout, Hash hash, MPI_Datatype Type, MPI_Comm C
     {
         std::cout << " Proc [" << rank << "] has " << countVec[i] << " elements for Proc [" << i << "]\n";
     }
-
     MPI_Barrier(Comm); // DEBUG: Step 2
+
 
     // 3. Sort inout based on hash % p so that displacement can be calculated
     // std::sort(inout.begin(), inout.end(), std::bind(hash_compare, std::placeholders::_1, std::placeholders::_2, hash, size));
@@ -86,22 +90,70 @@ void mpi_shuffle(std::vector<T>& inout, Hash hash, MPI_Datatype Type, MPI_Comm C
     std::sort(inout.begin(), inout.end(), [size, hash](T &x, T &y){return ((hash(x) % size)) < ((hash(y) % size));});
 
     std::cout << "First element: " << inout[0] << " Last element: " << inout[inout.size() - 1] << " on processor " << rank << "\n";
-
-    for(int i = 0; i < countVec.size(); i++)
-    {
-        std::cout << " Proc [" << rank << "] has " << displacementVec[i] << " elements for Proc [" << i << "]\n";
-    }
     MPI_Barrier(Comm); // DEBUG: Step 3
-    // for(int i = 0; i < n; i++)
-    // {
-    //     if(((rank + i) % 2) == 0)
-    //     {
-    //         if(rank < n-2)
-    //         {
-    //             MPI_Sendrecv_replace(inout, 1, MPI_INT, rank, rank, rank + 1, rank + 1, Comm);
-    //         }
-    //     }
-    // }
+
+    // 4. Calculate displacement in inout for each other processor to send
+
+    for(int i=0, mod=0; i < inout.size(); i++)
+    {
+        if (( hash(inout[i]) % size) == mod)
+        {
+            continue;
+        }
+        else
+        {
+            mod += 1;
+            displacementVec[mod] = i;
+        }
+    }
+    for(int i = 0; i < displacementVec.size(); i++)
+    {
+        std::cout << " Proc [" << rank << "] has elements at " << displacementVec[i] << " for Proc [" << i << "]\n";
+    }
+    MPI_Barrier(Comm); // DEBUG: Step 4
+    //TODO: Merge for-loops in Step 2 and Step 4 into one
+
+
+    // 5. Send and receive the no of items for each processor to be received
+    MPI_Status status;
+    for(int i = 0; i <size; i++)
+    {
+        MPI_Sendrecv(&countVec[i], 1, MPI_INT, i, i,
+                &recCountVec[i], 1, MPI_INT, i, rank, Comm, &status);
+    }
+
+    for(int i=0; i<size; i++)
+    {
+        std::cout << "Proc [" << rank << "] received " << recCountVec[i] << " from Proc [" << i << "]\n";
+    }
+    MPI_Barrier(Comm); // DEBUG: Step 5
+
+    // Extra: calculate total items to be received and sent
+    for (int i = 0; i<recCountVec.size(); i++)
+    {
+        totalRecCount += recCountVec[i];
+        totalSendCount += countVec[i];
+    }
+
+    std::cout << "Proc [" << rank << "] will send total " << totalSendCount << " elements\n";
+    std::cout << "Proc [" << rank << "] will receive total " << totalRecCount << " elements\n";
+
+    // 6. Prepare the displacement vector for storing the offsets when receiving
+    std::vector<int> recDisplacement(size, 0);
+
+    for (int i = 0, sum = 0; i < size; i++)
+    {
+        recDisplacement[i] = sum;
+        sum += recCountVec[i]; // Sum of items to receive till that point is the displacement
+    }
+
+    for (int i = 0; i<size; i++)
+    {
+        std::cout << "Displacement on [" << rank << "] for receiving from " << i << " is " << recDisplacement[i] << "\n";
+    }
+    MPI_Barrier(Comm);
+
+    // TODO: Check if new buffer is needed for alltoallv and perform alltoallv()
 } // mpi_shuffle
 
 #endif // A1_HPP
