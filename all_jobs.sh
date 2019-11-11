431855a16ef1a413c4de0b240a0bb619431f73a9
Abhyudaya <amourya@srv-p22-12.cbls.ccr.buffalo.edu>
2019-11-11 16:28:00 -0500

    Final commit for A1
    - Add job scripts for CCR
    - Add result benchmarks (that are admittedly shitty for some reason)

---
+++ all_jobs.sh
@@ -0,0 +1,4 @@
+sbatch --ntasks-per-node=2 --job-name="mpi-shuffle" job.sh
+sbatch --ntasks-per-node=4 --job-name="mpi-shuffle" job.sh
+sbatch --ntasks-per-node=6 --job-name="mpi-shuffle" job.sh
+sbatch --ntasks-per-node=8 --job-name="mpi-shuffle" job.sh
---
+++ job_general.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+###### select partition (check CCR documentation)
+#SBATCH --partition=general-compute --qos=general-compute
+
+####### set memory that nodes provide (check CCR documentation, e.g., 32GB)
+#SBATCH --mem=48000
+
+####### make sure no other jobs are assigned to your nodes
+#SBATCH --exclusive
+
+####### further customizations
+#SBATCH --job-name="mpi-shuffling"
+#SBATCH --output=results/%j.stdout
+#SBATCH --error=results/%j.stderr
+#SBATCH --nodes=2
+##SBATCH --ntasks-per-node=1
+#SBATCH --time=00:15:00
+##SBATCH --mail-type=END
+##SBATCH --mail-user=amourya@buffalo.edu
+
+export I_MPI_DEBUG=4
+export I_MPI_PMI_LIBRARY=/usr/lib64/libpmi.so
+module load intel-mpi/2018.3
+
+srun -n 4 ./a1
